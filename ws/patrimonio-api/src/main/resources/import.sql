2eea71a582a8b13a3a9e0611d1e01bea27de5579
felipejsm <felipejsm41@gmail.com>
2018-04-02 22:34:09 -0300

    Criado arquivo sql com 2 inserts

---
+++ ws/patrimonio-api/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into item (etiqueta, descricao, data_aquisicao) values ('NT0123', 'notebook i7', sysdate());
+insert into item (etiqueta, descricao, data_aquisicao) values ('AU9875', 'Ford Fiesta', sysdate());
